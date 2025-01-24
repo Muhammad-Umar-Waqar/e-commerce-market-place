10d4b6b3c70ccc5c8e31629a22906b3ad7ec54f6
Muhammad Umar Waqar <mohammthegreat@gmail.com>
2025-01-24 22:31:59 +0500

    Added All Products From SSanity and Price Range Filteration

--- src/components/product-page/Bars/ReviewsContent.tsx
+++ src/components/product-page/Bars/ReviewsContent.tsx
@@ -8,8 +8,8 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import ReviewCard from "@/components/reuse/ReviewCard";
-import { reviewsData } from "@/app/page";
 import Link from "next/link";
+import { reviewsData } from "@/lib/utils";
 
 const ReviewsContent = () => {
   return (
